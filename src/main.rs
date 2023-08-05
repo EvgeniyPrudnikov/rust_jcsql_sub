7c81599caaa9ad82faa6d559a0c6252d401f15b5
Evgeniy Prudnikov <e_prudnikov@wargaming.net>
2023-08-05 10:59:08 -0500

    upd

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,8 @@ use engines::impala::Impala;
 
 use crate::printing::{CellLines, CellParams, CellSize};
 
+use std::collections::VecDeque;
+
 fn main() -> Result<(), Error> {
     let a = Args::parse();
 
@@ -16,19 +18,48 @@ fn main() -> Result<(), Error> {
 
     let raw_query = a.get_query();
 
-    let (col_des, mut c) = i.execute(&raw_query, a.fetch_num)?;
+    let (col_desc, mut c) = i.execute(&raw_query, a.fetch_num)?;
     let data = i.fetch(&mut c, a.fetch_num)?;
 
-    let mut col_max: Vec<usize> = vec![0; col_des.len()];
-    upd_header_col_max_lens(&col_des, &mut col_max);
+    //------ process data ----------------
+    let print_buffer = to_print_buffer(&col_desc, &data);
+
+    for i in print_buffer {
+        printing::print_cells_line(i);
+    }
+
+    Ok(())
+}
+
+fn upd_data_col_max_lens(data_row: &[String], max_len: &mut [usize]) {
+    for (idx, i) in data_row.iter().enumerate() {
+        let chars_cnt = i.chars().count();
+        if chars_cnt > max_len[idx] {
+            max_len[idx] = chars_cnt
+        }
+    }
+}
+
+fn upd_header_col_max_lens(header_row: &[ColDesc], max_len: &mut [usize]) {
+    for (idx, i) in header_row.iter().enumerate() {
+        let chars_cnt = i.get_print_name().chars().count();
+        if chars_cnt > max_len[idx] {
+            max_len[idx] = chars_cnt
+        }
+    }
+}
+
+fn to_print_buffer(header: &Vec<ColDesc>, data: &Vec<Vec<String>>) -> Vec<Vec<CellLines>> {
+    let mut print_buffer: Vec<Vec<CellLines>> = Vec::new();
+    let mut col_max: Vec<usize> = vec![0; header.len()];
+
+    upd_header_col_max_lens(header, &mut col_max);
     for i in data.iter() {
         upd_data_col_max_lens(i, &mut col_max);
     }
 
-    let mut print_buffer: Vec<Vec<CellLines>> = Vec::new();
-
     let mut print_row: Vec<CellLines> = Vec::new();
-    for (col_idx, cd) in col_des.iter().enumerate() {
+    for (col_idx, cd) in header.iter().enumerate() {
         print_row.push(printing::cell_to_print(CellParams::new(
             cd.get_print_name(),
             cd.get_print_name().len(),
@@ -57,28 +88,5 @@ fn main() -> Result<(), Error> {
         }
         print_buffer.push(print_row);
     }
-
-    for i in print_buffer {
-        printing::print_cells_line(i);
-    }
-
-    Ok(())
-}
-
-fn upd_data_col_max_lens(data_row: &[String], max_len: &mut [usize]) {
-    for (idx, i) in data_row.iter().enumerate() {
-        let chars_cnt = i.chars().count();
-        if chars_cnt > max_len[idx] {
-            max_len[idx] = chars_cnt
-        }
-    }
-}
-
-fn upd_header_col_max_lens(header_row: &[ColDesc], max_len: &mut [usize]) {
-    for (idx, i) in header_row.iter().enumerate() {
-        let chars_cnt = i.get_print_name().chars().count();
-        if chars_cnt > max_len[idx] {
-            max_len[idx] = chars_cnt
-        }
-    }
+    print_buffer
 }
