8be91916493c2d1d07901596d338e4a0cc177253
Evgeniy Prudnikov <e_prudnikov@wargaming.net>
2023-08-05 11:44:00 -0500

    upd

--- src/main.rs
+++ src/main.rs
@@ -8,25 +8,46 @@ mod engines;
 use engines::impala::Impala;
 
 use crate::printing::{CellLines, CellParams, CellSize};
-
+use chrono::{Duration, Local};
 use std::collections::VecDeque;
+
+fn format_duration(duration: Duration) -> String {
+    let hours = duration.num_hours();
+    let minutes = duration.num_minutes() % 60;
+    let seconds = duration.num_seconds() % 60;
+    let milliseconds = duration.num_milliseconds() % 1000;
+
+    format!(
+        "{:02}:{:02}:{:02}:{:03}",
+        hours, minutes, seconds, milliseconds
+    )
+}
 
 fn main() -> Result<(), Error> {
     let a = Args::parse();
+    let mut start_msg: Vec<String> = Vec::new();
+    let mut end_msg: Vec<String> = Vec::new();
 
     let i = Impala::new(a.connection_string.clone());
+    start_msg.push(format!(
+        "[{}] Connected to {:?}",
+        Local::now().format("%Y-%m-%d %H:%M:%S"),
+        i.engine
+    ));
 
     let raw_query = a.get_query();
+    start_msg.push(raw_query.clone());
 
+    let start_time = chrono::Local::now();
     let (col_desc, mut c) = i.execute(&raw_query, a.fetch_num)?;
     let data = i.fetch(&mut c, a.fetch_num)?;
+    let duration = chrono::Local::now() - start_time;
+    end_msg.push(format!("Elapsed {} s", format_duration(duration)));
 
     //------ process data ----------------
     let print_buffer = to_print_buffer(&col_desc, &data);
-
-    for i in print_buffer {
-        printing::print_cells_line(i);
-    }
+    //------ print result ----------------
+    print_message(&start_msg, print_buffer, &end_msg);
 
     Ok(())
 }
@@ -46,6 +67,26 @@ fn upd_header_col_max_lens(header_row: &[ColDesc], max_len: &mut [usize]) {
         if chars_cnt > max_len[idx] {
             max_len[idx] = chars_cnt
         }
+    }
+}
+
+fn print_message(
+    start_msg: &Vec<String>,
+    print_buffer: Vec<Vec<CellLines>>,
+    end_msg: &Vec<String>,
+) {
+    for smsg in start_msg {
+        println!("{}\n", smsg);
+    }
+
+    let fetched = print_buffer.len();
+    for i in print_buffer {
+        printing::print_cells_line(i);
+    }
+    println!("\nFetched {} rows", fetched);
+
+    for emsg in end_msg {
+        println!("{}\n", emsg);
     }
 }
 
